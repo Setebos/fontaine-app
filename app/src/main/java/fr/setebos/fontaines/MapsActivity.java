c96eef8152196d3359bd2395ed551712682b957e
Setebos <moffjerjerrod@gmail.com>
2015-06-30 09:07:39 +0200

    Premier jet pour récupération des données webservice (incomplet)

--- app/src/main/java/fr/setebos/fontaines/MapsActivity.java
+++ app/src/main/java/fr/setebos/fontaines/MapsActivity.java
@@ -1,15 +1,19 @@
 package fr.setebos.fontaines;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.google.android.gms.location.LocationServices;
@@ -20,10 +24,30 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     GPSTracker gps;
     Button btnShowLocation;
+    private ProgressDialog pDialog;
+    // URL to get contacts JSON
+    private static String url = "https://download.data.grandlyon.com/wfs/grandlyon?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=200&request=GetFeature&typename=epo_eau_potable.epobornefont";
+
+    private static final String MARKER_ID = "gid";
+    private static final String MARKER_COORDINATES = "coordinates";
+
+    // contacts JSONArray
+    JSONArray markers = null;
+
+    // Hashmap for ListView
+    ArrayList<HashMap<String, String>> markersList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +56,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        markersList = new ArrayList<HashMap<String, String>>();
+        new GetMarkers().execute();
     }
 
     @Override
@@ -63,5 +89,96 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         });
+    }
+
+    /**
+     * Async task class to get json by making HTTP call
+     * */
+    private class GetMarkers extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MapsActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+
+            Log.d("Response: ", "> " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    markers = jsonObj.getJSONArray("features");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < markers.length(); i++) {
+                        JSONObject c = markers.getJSONObject(i);
+
+                        String id = c.getString(MARKER_ID);
+                        String coordinates = c.getString(MARKER_COORDINATES[0]);
+                        String email = c.getString(TAG_EMAIL);
+                        String address = c.getString(TAG_ADDRESS);
+                        String gender = c.getString(TAG_GENDER);
+
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject(TAG_PHONE);
+                        String mobile = phone.getString(TAG_PHONE_MOBILE);
+                        String home = phone.getString(TAG_PHONE_HOME);
+                        String office = phone.getString(TAG_PHONE_OFFICE);
+
+                        // tmp hashmap for single contact
+                        HashMap<String, String> contact = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put(TAG_ID, id);
+                        contact.put(TAG_NAME, name);
+                        contact.put(TAG_EMAIL, email);
+                        contact.put(TAG_PHONE_MOBILE, mobile);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList,
+                    R.layout.list_item, new String[] { TAG_NAME, TAG_EMAIL,
+                    TAG_PHONE_MOBILE }, new int[] { R.id.name,
+                    R.id.email, R.id.mobile });
+
+            setListAdapter(adapter);
+        }
+
     }
 }
